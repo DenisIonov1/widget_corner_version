3ecc9b4515adc879aeb33d0601289537ecce0baa
Денис Ионов <den.ionov4002@gmail.com>
2026-02-16 18:29:42 +1000

    add cross

--- widget_corner.js
+++ widget_corner.js
@@ -209,7 +209,32 @@
             flex-direction: column;
             background-color: #cccccc7a;
             border-radius: 16px;
+            padding-top: 28px;
             z-index: 2147483000;
+        }
+
+        .n7-widget__close-chat {
+            padding: 0;
+            width: 24px;
+            height: 24px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            right: 10px;
+            top: 2px;
+            background-color: transparent;
+            border: none;
+            cursor: pointer;
+            tramsition: all 0.2s;
+        }
+            
+        .n7-widget__close-chat:hover {
+            transform: scale(1.15);
+        }
+
+        .n7-widget__close-chat:active {
+            transform: scale(0.95);
         }
 
         .n7-widget__body {
@@ -220,7 +245,7 @@
             display: flex;
             flex-direction: column;
             row-gap: 10px;
-            padding: 15px;
+            padding: 0 15px;
             justify-content: flex-start;
             scrollbar-width: none;
         }
@@ -707,7 +732,12 @@
     wrapper.className = "n7-widget";
 
     wrapper.innerHTML = `
-        <div class="n7-widget__close"></div>
+        <button type="button" class="n7-widget__close-chat" aria-label="">
+            <svg class="n7-close-chat-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 13.4L7.09999 18.3C6.91665 18.4834 6.68332 18.575 6.39999 18.575C6.11665 18.575 5.88332 18.4834 5.69999 18.3C5.51665 18.1167 5.42499 17.8834 5.42499 17.6C5.42499 17.3167 5.51665 17.0834 5.69999 16.9L10.6 12L5.69999 7.10005C5.51665 6.91672 5.42499 6.68338 5.42499 6.40005C5.42499 6.11672 5.51665 5.88338 5.69999 5.70005C5.88332 5.51672 6.11665 5.42505 6.39999 5.42505C6.68332 5.42505 6.91665 5.51672 7.09999 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z"
+                    fill="#000000" />
+                </svg>
+        </button>
         <div class="n7-widget__body" role="log" aria-live="polite">
             <div class="n7-message n7-message--bot">
 
@@ -1253,12 +1283,17 @@
 
         const form = document.querySelector('.n7-form');
         const messageInput = document.querySelector('.n7-input');
+        const closeChat = document.querySelector('.n7-widget__close-chat');
 
         messageInput.addEventListener('keydown', (e) => {
             if (e.key === 'Enter' && !e.shiftKey && !e.ctrlKey && !e.metaKey) {
                 e.preventDefault();
                 form.requestSubmit();
             }
+        });
+
+        closeChat.addEventListener('click', () => {
+            wrapper.style.display = 'none';
         });
 
         window.addEventListener('online', () => {
